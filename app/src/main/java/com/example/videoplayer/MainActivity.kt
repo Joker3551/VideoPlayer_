633e309100f45ad3d575626f0550b06213857a8d
Joker3551 <MACHINIMA96@MAIL.RU>
2022-07-29 17:50:25 +1000

    4_Zadanie + Цикл между таймкодами

--- app/src/main/java/com/example/videoplayer/MainActivity.kt
+++ app/src/main/java/com/example/videoplayer/MainActivity.kt
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
 import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.google.android.exoplayer2.util.EventLogger
-import com.google.android.exoplayer2.util.FlagSet
 import com.google.android.exoplayer2.util.Log
 import com.google.common.collect.ImmutableList
 import com.google.android.exoplayer2.Player
@@ -59,6 +57,7 @@ class MainActivity : AppCompatActivity(), Player.Listener {
         player.addListener(this)
         TimeCodes()
         TimeAcc()
+        Cycle()
 
     }
 
@@ -117,6 +116,42 @@ class MainActivity : AppCompatActivity(), Player.Listener {
         button5.setOnClickListener{
             player.setPlaybackSpeed(2.0F)
         }
+    }
+
+    private fun Cycle(){
+        val table: Array<Array<Int>> = Array(7,{ Array(2,{0}) })
+        table[0]= arrayOf(0,10000)
+        table[1]= arrayOf(10000,60000)
+        table[2]= arrayOf(60000,180000)
+        table[3]= arrayOf(180000,300000)
+        table[4]= arrayOf(300000,480000)
+        table[5]= arrayOf(480000,600000)
+        table[6]= arrayOf(600000,654000)
+        player.removeListener(this)
+
+        val button: Button = findViewById(R.id.cycle)
+        button.setOnClickListener{
+            val curTime = player.currentPosition
+            for(row in table){
+                if(curTime>row[0]&&row[1]>curTime){
+                    button.visibility = View.INVISIBLE
+
+                    player.addListener(object : Player.Listener {
+                        override fun onEvents(player: Player, events: Player.Events) {
+                            super.onEvents(player, events)
+                            if(player.currentPosition>=row[1] || player.currentPosition<row[0]){
+                                player.seekTo(row[0].toLong())
+                            }
+                        }
+
+                    })
+
+                }
+
+            }
+
+        }
+
     }
 
     override fun onStop() {
